e9ae8a8717a030794f4dde8eb74247cb58c19d26
unknown <frank@kaputasystems.com>
2020-05-12 23:06:09 -0400

    switch statement mostly

--- AJAXTemplate.js
+++ AJAXTemplate.js
@@ -3,20 +3,24 @@
 function do_update() {
     xmlhttp=new XMLHttpRequest();
 	xmlhttp.onreadystatechange=function() {
-        // we only look for readyState of 4 (DONE), but could process others
+        // we only look for readyState of 4 (DONE), but could process the others:
+        // 0 UNSENT 	        Client has been created. open() not called yet.
+        // 1 OPENED 	        open() has been called.
+        // 2 HEADERS_RECEIVED 	send() has been called, and headers and status are available.
+        // 3 LOADING 	        Downloading; responseText holds partial data.
 		if (xmlhttp.readyState==4) {
-            // a switch should be used here
-            // and we could look for other values besides 200 and 0
-			if (xmlhttp.status==200) {
-                // success
-                // look at xmlhttp.responseText
-			}
-			else if (xmlhttp.status== 0) { // if the server goes away, 0 is returned - unless a timeout happens first...
-				// handle the server going away
-			}
-			else { // not a success 
-				// handle this
-                // show the xmlhttp.status value
+            // and we could look for other readtState values besides 200 and 0
+            switch(xmlhttp.status) {
+                case 200: // success
+                    // look at xmlhttp.responseText
+                    break;
+                case 0: // if the server goes away, 0 is returned(?) - unless a timeout happens first...
+                    // how is this different from a timeout?  
+                    // handle the server going away
+                    break;
+                default: // not a success 
+                    // handle this
+                    // show the xmlhttp.status and xmlhttp.statusText
 			}
 		}
 	}
