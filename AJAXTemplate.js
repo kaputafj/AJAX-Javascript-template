be65c8e3cf4f9eba98290df06e6bceae42f3d9f9
Frank Kaputa <frank@kaputasystems.com>
2020-11-09 08:34:32 -0800

    Update AJAXTemplate.js
    formatting -replaced tabs with spaces

--- AJAXTemplate.js
+++ AJAXTemplate.js
@@ -2,13 +2,13 @@
 
 function do_update() {
     xmlhttp=new XMLHttpRequest();
-	xmlhttp.onreadystatechange=function() {
+    xmlhttp.onreadystatechange=function() {
         // we only look for readyState of 4 (DONE), but could process the others:
         // 0 UNSENT 	        Client has been created. open() not called yet.
         // 1 OPENED 	        open() has been called.
         // 2 HEADERS_RECEIVED 	send() has been called, and headers and status are available.
         // 3 LOADING 	        Downloading; responseText holds partial data.
-	if (xmlhttp.readyState==4) {
+        if (xmlhttp.readyState==4) {
             // and we could look for other readyState values besides 200 and 0
             switch(xmlhttp.status) {
                 case 200: // success
@@ -22,15 +22,15 @@ function do_update() {
                 default: // not a success 
                     // handle this
                     // show the xmlhttp.status and xmlhttp.statusText
-			}
-		}
-	}
-	xmlhttp.ontimeout = function () {
-  		// XMLHttpRequest timed out. 
+            }
+        }
+    }
+    xmlhttp.ontimeout = function () {
+        // XMLHttpRequest timed out. 
         // handle it
-	};
-	
-	xmlhttp.open("GET","<my URL / file>",true);
-	xmlhttp.timeout = 5000; // milliseconds - must be after open() call for IE
-	xmlhttp.send();
+    };
+ 
+    xmlhttp.open("GET","<my URL / file>",true);
+    xmlhttp.timeout = 5000; // milliseconds - must be after open() call for IE
+    xmlhttp.send();
 }
